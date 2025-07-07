dc4e1239bbb02d6308993d6e04e573f0aca3cc2d
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-07 18:35:13 +0200

    03_mocks/01_traits

--- exercises/03_mocks/01_traits/src/lib.rs
+++ exercises/03_mocks/01_traits/src/lib.rs
@@ -3,28 +3,40 @@
 //! Then pass a `TestLogger` to `square` in the test. `TestLogger` should implement `Logger` and do nothing
 //! when `log` is called.
 
-pub fn square(x: i32, logger: PrintlnLogger) -> i32 {
-    let y = x * x;
-    logger.log(&format!("{}^2 == {}", x, y));
-    y
+pub trait Logger {
+    fn log(&self, msg: &str);
 }
 
 pub struct PrintlnLogger;
 
-impl PrintlnLogger {
-    pub fn log(&self, msg: &str) {
-        println!("{}", msg);
+
+pub fn square<L: Logger>(x: i32, logger: L) -> i32 {
+    let y = x * x;
+    logger.log(&format!("{x}^2 == {y}"));
+    y
+}
+
+
+impl Logger for PrintlnLogger {
+    fn log(&self, msg: &str) {
+        println!("{msg}");
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::square;
+    use super::{square, Logger};
     use googletest::assert_that;
     use googletest::matchers::eq;
 
+    struct TestLogger;
+
+    impl Logger for TestLogger {
+        fn log(&self, _msg: &str) {}
+    }
+
     #[test]
     fn square_works() {
-        assert_eq!(square(2, todo!()), 4);
+        assert_that!(square(2, TestLogger), eq(4));
     }
 }
