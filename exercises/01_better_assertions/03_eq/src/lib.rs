0080258621d91399353811d577cea0aa3ba2f904
Michael Kaiser <michael.kaiser@gfn.education>
2025-07-07 14:16:10 +0200

    01_better_assertions/03_eq

--- exercises/01_better_assertions/03_eq/src/lib.rs
+++ exercises/01_better_assertions/03_eq/src/lib.rs
@@ -4,34 +4,33 @@
 //! Notice how `googletest` sees through variable names and prints the actual values that were compared!
 #[cfg(test)]
 mod tests {
-    use googletest::assert_that;
-    use googletest::matchers::{empty, eq, gt, lt, not};
+    use googletest::prelude::*;
 
     #[googletest::gtest]
     fn failed_eq() {
         let x = 1;
         let y = 2;
-        assert_eq!(x, y);
+        assert_that!(x, eq(y));
     }
 
     #[googletest::gtest]
     fn failed_neq() {
         let x = 1;
         let y = 1;
-        assert_ne!(x, y);
+        assert_that!(x, not(eq(y)));
     }
 
     #[googletest::gtest]
     fn failed_greater_than() {
         let x = 3;
         let y = 4;
-        assert!(x > y);
+        assert_that!(x, gt(y));
     }
 
     #[googletest::gtest]
     fn failed_less_than() {
         let x = 10;
         let y = -1;
-        assert!(x < y);
+        assert_that!(x, lt(y));
     }
 }
